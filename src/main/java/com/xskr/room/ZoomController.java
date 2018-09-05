0aed06a95b12e2335ae72923e8d4ee23bb137316
unknown <tlw_ray@163.com>
2018-09-05 17:04:31 +0800

    update

--- src/main/java/com/xskr/room/ZoomController.java
+++ src/main/java/com/xskr/room/ZoomController.java
@@ -1,9 +1,23 @@
 package com.xskr.room;
 
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.jws.WebMethod;
+
+@RestController
 public class ZoomController {
 
+    @RequestMapping(value = "/zoom", method = RequestMethod.POST)
+    public void createXxxZoom(){
+
+    }
+
+    @RequestMapping("/zoom/{uuid}")
+    public void joinZoom(@PathVariable String uuid){
+
+    }
 }
